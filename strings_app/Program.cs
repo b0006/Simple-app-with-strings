05b23baf1de92ff00a8821e69a4eb3c8bc83a598
b0006 <qsefthuken@gmail.com>
2017-10-07 20:33:11 +0700

    Added project files

---
+++ strings_app/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace strings_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextReader reader = new StreamReader("TextFile.txt", Encoding.Default);
+            string s = reader.ReadToEnd();
+            Console.WriteLine(s);
+            Console.WriteLine("\n");
+
+            string[] par = s.Split('!', '.', '?');
+
+            for (int i = 0; i < par.Length; i++)
+            {
+                if (par[i].IndexOf(',') == -1)
+                {
+                    Console.WriteLine(par[i].Trim());
+                }
+            }
+            reader.Close();
+            Console.ReadKey();
+        }
+    }
+}
+
+
